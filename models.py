f072d32aeb08cbb1057eda3fadc2785fc434cb4f
Gavin Leith <gavinleith@utexas.edu>
2016-03-17 09:40:43 -0500

    Update models.py
    Other models added in. Need to put in unit tests for these

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 # models.py
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import MetaData, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import delcarative_base
 
 Base = declarative_base()
@@ -9,24 +9,70 @@ class Meteorite(Base):
 	__tablename__ = 'meteorite'
 
 	id = Column(Integer, primary_key=True)
-	mass = Column(Integer, unique=True)
-	nametype = Column(String(50), unique=True)
-	recclass = Column(String(50), unique=True)
-	fall = Column(String(10), unique=True)
+	mass = Column(Integer)
+	nametype = Column(String(50))
+	recclass = Column(String(50))
+	fall = Column(String(10))
 	name = Column(String(50), unique=True)
-	reclong = Column(Float, unique=True)
-	reclat = Column(Float, unique=True)
+	year = Column(String)
+	reclong = Column(Float)
+	reclat = Column(Float)
 
 	def __init__(self, mass = 0, nametype = None, recclass = None,
-	 fall = None, name = None, reclong = 0.0, reclat = 0.0):
+	 fall = None, name = None, year = None, reclong = 0.0, reclat = 0.0):
 		self.mass = mass
 		self.nametype = nametype
 		self.recclass = recclass
 		self.fall = fall
 		self.name = name
+		self.year = year
 		self.reclong = reclong
 		self.reclat = reclat
 
 	def __repr__(self):
-		return'<Meteorite %r>' % (self.name)
+		return '<Meteorite %r>' % (self.name)
 
+
+class Classifications(Base):
+	__tablename__ = 'Classifications'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String)
+	compositionalType = Column(String)
+	parentBody = Column(String)
+	numberFound = Column(Integer)
+
+	def __init__(self, name = None, compositionalType = None, parentBody = None,
+	 numberFound = 0):
+		self.name = name
+		self.compositionalType = compositionalType
+		self.parentBody = parentBody
+		self.numberFound = numberFound
+
+	def __repr__(self):
+		return '<Classifications %r>' % (self.name)
+
+class Location(Base):
+	__tablename__ = 'Location'
+
+	id = Column(Integer, primary_key=True)
+	country = Column(String)
+	earliestYear = Column(Integer)
+	lowestLong = Column(Float)
+	lowestLat = Column(Float)
+	highestLong = Column(Float)
+	highestLat = Column(Float)
+
+	def __init__(self, country = None, earliestYear = 0, lowestLat = 0.0,
+	lowestLong = 0.0, highestLat = 0.0, highestLong = 0.0)
+		self.country = country
+		self.earliestYear = earliestYear
+		self.lowestLong = lowestLong
+		self.lowestLat = lowestLat
+		self.highestLong = highestLong
+		self.highestLat = highestLat
+
+	def __repr__(self):
+		return '<Location %r>' % (self.country)
+	
+		
