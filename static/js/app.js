e8d5d5466af3304ae2f50556848017bdc3cf1288
Jlavatai <jordanlavatai@gmail.com>
2016-03-24 12:21:07 -0500

    switched routing to ui-router for nested and state-based views

--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,51 @@
 // app.js
-var meteoriteApp = angular.module('meteoriteApp', ['ngRoute']);
+var meteoriteApp = angular.module('meteoriteApp', ['ui.router']);
 
-meteoriteApp.config(function($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl : 'static/partials/splash.html',
-            controller: 'splashController'
-        })
-        .when('/locations', {
-            templateUrl : 'static/partials/locations.html',
-            controller  : 'locationsController'
-        })
-        .when('/meteorites', {
-            templateUrl : 'static/partials/meteorites.html',
-            controller  : 'meteoritesController'
-        })
-        .when('/classifications', {
-            templateUrl : 'static/partials/classifications.html',
-            controller  : 'classificationsController'
-        })
-        .when('/about', {
-            templateUrl : 'static/partials/about.html',
-            controller  : 'aboutController'
-        });
-
-    $locationProvider.html5Mode(true);
+meteoriteApp.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+      .state('splash', {
+          url: '/',
+          templateUrl: "static/partials/splash.html"
+      })
+      .state('meteorites', {
+        url: "/meteorites",
+        templateUrl: "static/partials/meteorites.html",
+        controller: 'meteoritesController'
+      })
+      .state('locations', {
+        url: "/locations",
+        templateUrl: "static/partials/locations.html",
+        controller: 'locationsController'
+      })
+      .state('about', {
+          url: "/about",
+          templateUrl: 'static/partials/about.html',
+          controller: 'aboutController'
+      })
+      .state('classifications', {
+        url: "/classifications",
+        templateUrl: "static/partials/classifications.html",
+        controller: 'classificationsController'
+      });
+  //$locationProvider.html5Mode(true);
 });
-
-// create the controller and inject Angular's $scope
-
 meteoriteApp.controller('mainController', function($scope) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
 });
 
+// create the controller and inject Angular's $scope
+//
+//meteoriteApp.controller('mainController', function($scope) {
+//    // create a message to display in our view
+//    $scope.message = 'Everyone come and see how good I look!';
+//});
+//
 meteoriteApp.controller('splashController', function($scope) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
