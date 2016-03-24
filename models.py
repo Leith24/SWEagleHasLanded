c653e38188bb005df04747d77d4533040b63ee03
Adam Thompson <adamthompson@utexas.edu>
2016-03-24 15:14:53 -0500

    Models and UMLs
    Added function that creates country when one doesn't exist for added
    meteorite

--- models.py
+++ models.py
@@ -2,77 +2,122 @@
 
 from sqlalchemy import MetaData, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
+from geopy.geocoders import Nominatim
 
 Base = declarative_base()
+geolocator = Nominatim()
 
 class Meteorite(Base):
 	__tablename__ = 'meteorite'
+	_instances = set()
 
 	id = Column(Integer, primary_key=True)
 	mass = Column(Integer)
-	nametype = Column(String(50))
 	recclass = Column(String(50))
 	fall = Column(String(10))
 	name = Column(String(50), unique=True)
 	year = Column(String)
-	reclong = Column(Float)
-	reclat = Column(Float)
+	location = Column(String)
 
-	def __init__(self, mass = 0, nametype = None, recclass = None, 
-	fall = None, name = None, year = None, reclong = 0.0, reclat = 0.0):
-		self.mass = mass
-		self.nametype = nametype
-		self.recclass = recclass
-		self.fall = fall
+	def __init__(self, mass = 0, recclass = None, name = None, year = None, reclong = 0.0, reclat = 0.0, geolocation = "0.0, 0.0"):
 		self.name = name
+		self._instances.add(weakref.ref(self))
+		self.mass = mass
+		self.recclass = recclass
 		self.year = year
 		self.reclong = reclong
 		self.reclat = reclat
+		self.location = Location.addMeteorite(self)
 
 	def __repr__(self):
 		return '<Meteorite %r>' % (self.name)
 
+	@classmethod
+    def getinstances(cls):
+        dead = set()
+        for ref in cls._instances:
+            obj = ref()
+            if obj is not None:
+                yield obj
+            else:
+                dead.add(ref)
+        cls._instances -= dead
 
 class Classifications(Base):
 	__tablename__ = 'Classifications'
+	_instances = set()
 
 	id = Column(Integer, primary_key = True)
 	name = Column(String)
-	compositionalType = Column(String)
+	composition = Column(String)
 	parentBody = Column(String)
 	numberFound = Column(Integer)
+	meteorites = Column(String)
 
-	def __init__(self, name = None, compositionalType = None, parentBody = None,
-	 numberFound = 0):
+	def __init__(self, name = None, compositionalType = None, parentBody = None):
 		self.name = name
-		self.compositionalType = compositionalType
+		self._instances.add(weakref.ref(self))
+		self.composition = compositionalType
 		self.parentBody = parentBody
 		self.numberFound = numberFound
+		self.meteorites = []
+
 
 	def __repr__(self):
 		return '<Classifications %r>' % (self.name)
 
+	@classmethod
+    def getinstances(cls):
+        dead = set()
+        for ref in cls._instances:
+            obj = ref()
+            if obj is not None:
+                yield obj
+            else:
+                dead.add(ref)
+        cls._instances -= dead
+
 class Location(Base):
 	__tablename__ = 'Location'
+	_instances = set()
 
 	id = Column(Integer, primary_key=True)
 	country = Column(String)
 	earliestYear = Column(Integer)
-	lowestLong = Column(Float)
-	lowestLat = Column(Float)
-	highestLong = Column(Float)
-	highestLat = Column(Float)
+	numberFound = Column(Integer)
 
-	def __init__(self, country = None, earliestYear = 0, lowestLat = 0.0,
-	lowestLong = 0.0, highestLat = 0.0, highestLong = 0.0):
-		self.country = country
-		self.earliestYear = earliestYear
-		self.lowestLong = lowestLong
-		self.lowestLat = lowestLat
-		self.highestLong = highestLong
-		self.highestLat = highestLat
+	def __init__(self, country = None, meteorite):
+		self.name = country6
+		self._instances.add(weakref.ref(self))
+		self.meteorites = [meteorite]
+		numberFound =  len(meteorites)
+
 
 	def __repr__(self):
 		return '<Location %r>' % (self.country)
-	
-		
+
+	@classmethod
+    def getinstances(cls):
+        dead = set()
+        for ref in cls._instances:
+            obj = ref()
+            if obj is not None:
+                yield obj
+            else:
+                dead.add(ref)
+        cls._instances -= dead
+
+	def add_meteorite(meteorite)
+		location = meteorite.geolocator.reverse(geolocation)
+		location = location.address.split(',')
+		name = Location.addMeteorite(location[-1])
+		exists = False
+		for loc in Location.getinstances():
+			if(loc.name == name)
+				exists = obj
+				break
+		if exists is not False
+			exists.meteorites += meteorite
+		else
+			exists = Location(name, meteorite)
+		return exists
