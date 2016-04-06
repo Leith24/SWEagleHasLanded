3922e75a8b9606afc866f9e513bb2a2a80cfae29
Jlavatai <jordanlavatai@gmail.com>
2016-04-05 22:06:44 -0500

    meteorites + meteorite

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -2,15 +2,19 @@ meteoriteApp.controller('mainController', function($scope) {
     // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
 });
-
+//DATA TABLE SORTING WILL BE BROKEN ON INTEGERS NEED TO FIX IN DATABASE
 meteoriteApp.controller('meteoritesController', function($scope, meteoritesObj) {
-    $scope.message = 'meteoritesObj';
-    console.log(meteoritesObj);
+    $scope.sortType     = 'name'; // set the default sort type
+    $scope.sortReverse  = false;  // set the default sort order
+    $scope.searchMeteorites   = '';     // set the default search/filter term
+
     $scope.meteorites = meteoritesObj.data;
 });
 
 meteoriteApp.controller('meteoriteController', function($scope, $stateParams, meteoriteObj) {
-    $scope.meteorite = meteoriteObj;
+    $scope.meteorite = meteoriteObj.data;
+    //TODO: Get country from geolocation
+    //$scope.country = geolocation bs
     $scope.message = $stateParams;
 
 });
