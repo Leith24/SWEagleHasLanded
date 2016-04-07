bce976bd882b8c1fdb7c4c102e9522b1d893cbd5
Dazmurphy <daz.murphy.94@gmail.com>
2016-04-07 15:11:53 -0600

    added populateCountries() method

--- app/populate.py
+++ app/populate.py
@@ -11,6 +11,24 @@ def populateMeteorites(meteorites_json_data):
         db.session.commit()
 
 def populateCountries(countries_json_data):
+    json_object = json.load(countries_json_data)
+
+    for ind_country in json_object:
+        #queries
+        meteorites_count = db.session.query(Meteorite).filter(Meteorite.country == ind_country['name']).count()
+
+        largest_year = db.session.query(db.func.max(Meteorite.year))
+        recent_meteorite = db.session(Meteorite).filter(Meteorite.year == largest_year).filter(Meteorite.year == ind_country['name']).all()
+
+        #model creation
+        country_model = Country(ind_country['name'], ind_country['area'], ind_country['latlng'], recent_meteorite, meteorites_count)
+        db.session.add(country_model)
+        db.session.commit()
 
 
 def populateClassifications(classifications_json_data):
+    json_object = json.load(classifications_json_data)
+
+    for ind_classifications in json_object:
+        classifications_model = Classification()
+
