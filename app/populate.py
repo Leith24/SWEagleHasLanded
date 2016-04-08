62778378da7ea6325a68a5a312c6e5988877500d
Adam Thompson <adamthompson@utexas.edu>
2016-04-07 21:34:25 -0500

    Adding intertable relationships

--- app/populate.py
+++ app/populate.py
@@ -6,7 +6,7 @@ def populateMeteorites(meteorites_json_data):
     meteorites_json_object = json.load(meteorites_json_data)
 
     for ind_meteorite in meteorites_json_object:
-        meteorite_model = Meteorite(ind_meteorite['name'], ind_meteorite['id'], ind_meteorite['recclass'], ind_meteorite['year'], ind_meteorite['reclong'], ind_meteorite['reclat'])
+        meteorite_model = Meteorite(ind_meteorite['name'], ind_meteorite['mass'], ind_meteorite['recclass'], ind_meteorite['year'], ind_meteorite['reclat'], ind_meteorite['reclong'])
         db.session.add(meteorite_model)
         db.session.commit()
 
@@ -33,3 +33,33 @@ def populateClassifications(classifications_json_data):
         classifications_model = Classification(ind_classifications, ind_classifications['Class_ID'], ind_classifications['Compositional_Type'], ind_classifications[''])
         db.session.add(classifications_model)
         db.session.commit()
+
+def create_tracks_album(countries_json_data):
+
+    countries_json_object = json.load(countries_json_data)
+    for country in countries_json_object:
+        for meteorite_model in Meteorite.query.filter(Meteorite.country == country['name']):
+            if meteorite_model == None:
+                i=1
+            else:
+                for track in album['tracks']['items']:
+                    artist_name=''
+                    count=0
+                    for artist in track['artists']:
+                        if count >=1:
+                            artist_name+=', '
+                        artist_name+=artist['name']
+                        count=count+1
+                    track_exist= Track.query.filter(Track.title == track['name']).first()
+                    if track_exist ==None:
+
+                        tracks_model=Track(track['name'],artist_name,album_db.release_date,album_db.name,album_db.images,track['duration_ms'],track['uri'],track['id'],album_db.id,album_db.col_img,track['href'])
+                        artist_in_track= re.split(', ', artist_name)
+                        for art_tr in artist_in_track:
+                            artist= Artist.query.filter(Artist.name == art_tr).first()
+                            if artist ==None:
+                                i=1
+                            else:
+                                tracks_model.artists2.append(artist)
+                        db.session.add(tracks_model)
+                        db.session.commit()
