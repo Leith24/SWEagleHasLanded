62330a43699a2af445a269deaadec8aefe858020
Adam Thompson <adamthompson@utexas.edu>
2016-04-19 16:38:43 -0500

    Minor change before sync

--- app/search.py
+++ app/search.py
@@ -1,9 +1,21 @@
+import re
+import collections
 from models import Meteorite, Classification, Country
-#Two options: Either we grab all the information form the models
+from db import db
+
+#Two options: Either we grab all the information from the models
 #here with model.query.all() and parse the information and return the result
 #OR
 #We do individual model calls on each field
 #Its about the same work I think, its just one makes a bunch of database calls
-#and the other makes 3 databases calls and the figures out the rest.
+#and the other makes 3 databases calls and the figures out the rest. 
 
+def parse_query(query):
+	terms = query.split(' ')
+
+def search(terms):
+	m = Meteorite.query.all()
+	c = Country.query.all()
+	cl = Classification.query.all()
+	
 
