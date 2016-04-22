1048fe365abeddc49a90f053b92d0cbb102ca3d1
Dazmurphy <daz.murphy.94@gmail.com>
2016-04-21 19:55:12 -0500

    fixed css for searchbar

--- app/db.py
+++ app/db.py
@@ -9,21 +9,11 @@ app = Flask(__name__)
 
 manager = Manager(app)
 
-<<<<<<< HEAD
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:O6xu3W2HCvK656@127.0.0.1:3306/test_models'
-# SQLALCHEMY_DATABASE_URI = \
-#     '{engine}://{username}:{password}@{hostname}/{database}'.format(
-#         engine='mysql+pymysql',
-#         username=os.getenv('MYSQL_USER'),
-#         password=os.getenv('MYSQL_PASSWORD'),
-#         hostname=os.getenv('MYSQL_HOST'),
-#         database=os.getenv('MYSQL_DATABASE'))
-=======
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root-password@172.99.70.79/meteor'
 # app.config['DEBUG'] = True
->>>>>>> 1c33f8ad2cf9df14d1a321071854d258fbbcabf3
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@127.0.0.1:3306/test_models'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:O6xu3W2HCvK656@127.0.0.1:3306/test_models'
+"""
 SQLALCHEMY_DATABASE_URI = \
     '{engine}://{username}:{password}@{hostname}/{database}'.format(
         engine='mysql+pymysql',
@@ -35,6 +25,7 @@ SQLALCHEMY_DATABASE_URI = \
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+"""
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
 db.create_all()
 
